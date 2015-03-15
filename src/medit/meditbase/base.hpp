1dcc63a5613568835e1d463b20f68ac7ed5c7903
Petr Jindra <el.mordo@gmail.com>
2015-03-15 14:40:04 +0100

    windows DLL special header + gitignore update

---
+++ src/medit/meditbase/base.hpp
@@ -0,0 +1,20 @@
+/*
+ * base.hpp
+ *
+ *  Created on: 15. 3. 2015
+ *      Author: petr
+ */
+
+#ifndef SRC_MEDIT_MEDITBASE_BASE_HPP_
+#define SRC_MEDIT_MEDITBASE_BASE_HPP_
+
+/*
+ * prefix for windows to inform compiler
+ * about class export to public interface of DLL
+ * TODO windows prefix
+ */
+#ifndef SHARED_EXPORT
+#define SHARED_EXPORT
+#endif
+
+#endif /* SRC_MEDIT_MEDITBASE_BASE_HPP_ */
